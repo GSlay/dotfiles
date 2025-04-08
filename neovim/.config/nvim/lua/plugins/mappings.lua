6bc54b498548fb63ed55c0df6c21215d510630e2
gslay <mha220871@gmail.com>
2025-04-08 09:39:34 +0700

    update neovim config 1

--- neovim/.config/nvim/lua/plugins/mappings.lua
+++ neovim/.config/nvim/lua/plugins/mappings.lua
@@ -1,26 +1,36 @@
 return {
+  -- Mappings (astrocore)
   {
     "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
     opts = {
       mappings = {
         i = {
-          ["<C-BS>"] = { "<C-w>", desc = "Delete previous word" },
+          ["<C-h>"] = { "<C-w>", desc = "Delete previous word" }, -- <C-h> equal <C-BS> in WSL
+          ["<C-j>"] = { "<Esc>o", desc = "Insert new line below from insert mode" }, -- <C-j> equal <C-CR> in WSL
         },
       },
     },
   },
+
+  -- LSP mappings (astrolsp)
   {
     "AstroNvim/astrolsp",
-    opts = function(_, opts) -- Ensure we merge options correctly
-      opts.mappings = opts.mappings or {}
-      opts.mappings.n = vim.tbl_extend("force", opts.mappings.n or {}, {
-        K = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
-        gD = {
-          function() vim.lsp.buf.declaration() end,
-          desc = "Declaration of current symbol",
-          cond = "textDocument/declaration",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          K = {
+            function() vim.lsp.buf.hover() end,
+            desc = "Hover symbol details",
+          },
+          gD = {
+            function() vim.lsp.buf.declaration() end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
         },
-      })
-    end,
+      },
+    },
   },
 }
