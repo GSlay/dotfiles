42dfc3491ce348d59320764335c29aa5e098a9d2
DucMinh2211 <minh.ha02211050@hcmut.edu.vn>
2025-04-23 14:21:16 +0700

    Create mycore.lua

---
+++ neovim/.config/nvim/lua/plugins/mycore.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = {
+        wrap = true,
+        tabstop = 4,
+        shiftwidth = 4,
+        expandtab = true,
+        softtabstop = 4,
+      },
+    },
+  },
+}
