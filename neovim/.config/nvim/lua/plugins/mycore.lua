fbfceb05acff3c53f964cebc2a68dacd233d98a9
DucMinh2211 <minh.ha02211050@hcmut.edu.vn>
2025-05-24 12:51:29 +0700

    Update mycore.lua

--- neovim/.config/nvim/lua/plugins/mycore.lua
+++ neovim/.config/nvim/lua/plugins/mycore.lua
@@ -6,11 +6,27 @@ return {
     options = {
       opt = {
         wrap = true,
-        tabstop = 4,
-        shiftwidth = 4,
+        tabstop = 2,
+        shiftwidth = 2,
         expandtab = true,
-        softtabstop = 4,
+        softtabstop = 2,
       },
     },
+  },
+  {
+    "LazyVim/LazyVim", -- Or whichever base plugin manages your core configuration
+    config = function()
+      -- Create an autocommand that triggers specifically for Python files
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "python", -- Apply this to files with filetype 'python'
+        callback = function()
+          -- Set local options for the current buffer
+          vim.opt_local.tabstop = 2
+          vim.opt_local.shiftwidth = 2
+          vim.opt_local.softtabstop = 2
+          vim.opt_local.expandtab = true -- Crucial: use spaces instead of tabs
+        end,
+      })
+    end,
   },
 }
