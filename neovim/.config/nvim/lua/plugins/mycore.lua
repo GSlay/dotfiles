e8ef1ab1707615651bb8da3f7d723a5d8a6ec22c
DucMinh2211 <minh.ha02211050@hcmut.edu.vn>
2025-05-24 13:24:56 +0700

    Update mycore.lua

--- neovim/.config/nvim/lua/plugins/mycore.lua
+++ neovim/.config/nvim/lua/plugins/mycore.lua
@@ -1,30 +1,30 @@
----@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    options = {
-      opt = {
-        wrap = true,
-        tabstop = 2,
-        shiftwidth = 2,
-        expandtab = true,
-        softtabstop = 2,
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      options = {
+        opt = {
+          wrap = true,
+          tabstop = 2,
+          shiftwidth = 2,
+          expandtab = true,
+          softtabstop = 2,
+        },
       },
     },
   },
   {
     "LazyVim/LazyVim", -- Or whichever base plugin manages your core configuration
     config = function()
-      -- Create an autocommand that triggers specifically for Python files
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "python", "rust" }, -- Apply this to files with filetype 'python'
+        pattern = { "python", "rust" },
         callback = function()
-          -- Set local options for the current buffer
           vim.opt_local.tabstop = 2
           vim.opt_local.shiftwidth = 2
           vim.opt_local.softtabstop = 2
-          vim.opt_local.expandtab = true -- Crucial: use spaces instead of tabs
+          vim.opt_local.expandtab = true
+          vim.opt_local.wrap = true
         end,
       })
     end,
