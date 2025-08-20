cb778b060e5f2df6e7cc4eb5a30a3c7735855534
GSlay <100353000+GSlay@users.noreply.github.com>
2025-08-21 01:06:44 +0700

    Update options.lua

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -8,6 +8,16 @@ vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
 vim.opt.shiftwidth = 4 -- Number of spaces for each indent
 vim.opt.expandtab = true -- Convert tabs to spaces
 
+-- Filetype-specific settings for Lua: 2 spaces
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 -- Enable line wrapping
 vim.opt.wrap = true -- Wrap lines when they exceed window width
 
