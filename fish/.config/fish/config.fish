760275314071f1e546945d2f44b483f696eab42f
GSlay <100353000+GSlay@users.noreply.github.com>
2025-08-31 00:20:19 +0700

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     bind \t accept-autosuggestion
     bind \cf complete
+    bind \ch backward-kill-word
 end
 
 #!/usr/bin/fish
