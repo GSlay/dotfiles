5737f982efe0e3f6d9fab2ced90540396582f34d
GSlay <100353000+GSlay@users.noreply.github.com>
2025-09-02 01:59:13 +0700

    Update md2light.lua

--- neovim/.config/nvim/lua/user/md2light.lua
+++ neovim/.config/nvim/lua/user/md2light.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_hl(0, "MarkdownQuote", { fg = "#FF9E3B", italic = false })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    -- Match anything inside double quotes, but not multiline
-    vim.fn.matchadd("MarkdownQuote", [["\zs[^"]\{-}\ze"]])
+    -- Matches any text inside double quotes (and the quotes themselves), including across multiple lines.
+    vim.fn.matchadd("MarkdownQuote", [[".\_.\{-}"]])
   end,
 })
