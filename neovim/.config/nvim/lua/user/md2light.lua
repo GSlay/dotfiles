882cd20c22d7fc61eed7d52028634885547b9b64
DucMinh2211 <minh.ha02211050@hcmut.edu.vn>
2025-09-04 13:28:25 +0700

    Update md2light.lua

--- neovim/.config/nvim/lua/user/md2light.lua
+++ neovim/.config/nvim/lua/user/md2light.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_hl(0, "MarkdownQuote", { fg = "#FF9E3B", italic = false })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    -- Matches any text inside double quotes (and the quotes themselves), including across multiple lines.
-    vim.fn.matchadd("MarkdownQuote", [[".\_.\{-}"]])
+    -- Matches any text inside double quotes (and the quotes themselves), on a single line, and handles escaped quotes.
+    vim.fn.matchadd("MarkdownQuote", [[\v"[^"\\]*(\\.)*[^"]*"]])
   end,
 })
