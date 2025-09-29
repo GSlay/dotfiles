46af8e6776614670d8c8164d81f2d44bc564fce6
DucMinh2211 <minh.ha02211050@hcmut.edu.vn>
2025-09-29 16:11:09 +0700

    Update lsp.lua

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,13 +2,19 @@ return {
   "neovim/nvim-lspconfig",
   ---@class PluginLspOpts
   opts = {
-    servers = {
-      -- clangd = {
-      --   mason = false,
-      -- },
-      -- lua_ls = {
-      --   mason = false,
-      -- }
-    },
+    -- servers = {
+    --   clangd = {
+    --     mason = false,
+    --   },
+    --   lua_ls = {
+    --     mason = false,
+    --   },
+    --   rust_analyzer = {
+    --     mason = false,
+    --   },
+    --   markdown_oxide = {
+    --     mason = false,
+    --   }
+    -- },
   },
 }
